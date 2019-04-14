ec5711f0c954e9f4930cd49db7cc45ac345b0eaa
Jaturong Kongmanee <jaturongkongmanee@Jaturongs-MacBook-Pro.local>
2019-04-14 15:53:11 -0500

    Broadcasting msg from server to clients.
    Just update the same msg.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 */
 
 /* #define is used to define CONSTANT */
-#define SIM_TIME 5.0
+#define SIM_TIME 20.0
 #define NUM_CLIENTS 2l
 #define NUM_SERVER 1l
 #define NUM_BOXES 3
@@ -31,20 +31,15 @@ struct nde node[NUM_CLIENTS + NUM_SERVER];
 
 
 struct message {
-    int id;
+    long id;
     /* represented by the number of update */
-    int last_updated_time;
+    /*int last_updated_time;*/
 
 };
 
 /* struct message data_list[100];*/
-
-long msg;
-
-MBOX mbox_arr[NUM_BOXES];
-
-
-TABLE resp_tm;
+struct message *msg;
+struct message *m;
 
 
 void init();
@@ -84,8 +79,8 @@ void init() {
 
     long all_node = NUM_CLIENTS + NUM_CLIENTS;
     max_mailboxes(all_node);
-    max_servers(all_node);
-    max_events(all_node);
+    /*max_servers(all_node);
+    max_events(all_node);*/
 
     for (i = 0; i < all_node; i++) {
         sprintf(str, "input.%d", i);
@@ -94,22 +89,25 @@ void init() {
 
     /*create_mailbox_set("mbox set", mbox_arr, NUM_BOXES);*/
 
-    resp_tm = table("msg rsp tm");
-
     server(0);
+    printf("Calling server\n");
+
     for (i = 0; i < NUM_CLIENTS; i++) {
         client(i);
-        printf("test %ld\n", i);
+        printf("Calling client %ld\n", i);
     }
-
     
 }
 
 void server(long n) {
     create("server");
-    printf("Server is generated");
+    printf("Server is generated\n");
+    invalidation_report();
     while(clock < SIM_TIME){
-        invalidation_report();
+        hold(5);
+        printf("Calling IR\n");
+        
+        
         /*update_data_items();
         receive_message();*/
     }   
@@ -117,18 +115,32 @@ void server(long n) {
 
 void invalidation_report() {
     create("ir");
-    printf("IR is generated");
+    printf("IR is generated\n");
     while(clock < SIM_TIME){
         hold(5);
         /* Broadcast: put the message into all clients' mailboxes */
+        /*struct message *msg;*/
+        msg = (struct message*)malloc(sizeof(*msg));
+            
+        /*send(node[0].mbox, (long)msg);
+        printf("sent message to node %ld\n", 0);
+        send(node[1].mbox, (long)msg);
+        printf("sent message to node %ld\n", 1);*/
+        /*struct message *m;
+        m = &msg;*/
+        /*receive(node[0].mbox, (long*)&m);
+        printf("message received %ld \n", node[0].mbox);
+        receive(node[1].mbox, (long*)&m);
+        printf("message received %ld \n", node[1].mbox);*/
         long i;
         for(i = 0; i < NUM_CLIENTS; i++) {
             /* Transmission delay */
             /* hold(2); */
-            long msg1;
-            msg1 = 5;
-            send(node[i].mbox, msg1);
-            printf("sent message to node %ld\n", i);
+            msg->id = i+1000;
+            send(node[i].mbox, (long)msg);
+            printf("sent message to node %ld with %ld\n", i, msg->id);
+            /*receive(node[i].mbox, (long*)&m);
+            printf("message received %ld \n", m->id);*/
         }
     }
     
@@ -138,6 +150,7 @@ void update_data_items() {
     create("update");
     while(clock < SIM_TIME){
         /* code */
+        hold(1);
     }
     
 }
@@ -146,6 +159,7 @@ void receive_message() {
     create("receive_message");
     while(clock < SIM_TIME){
         /**/
+        hold(1);
     }
 }
 
@@ -158,10 +172,12 @@ void receive_message() {
 
 void client(long n) {
     create("client");
-    printf("Client %ld is generatedddd", n);
+    printf("Client %ld is generatedddd\n", n);
+    receive_ir(n);
     while(clock < SIM_TIME){
+        hold(1);
         /*generate_query();*/
-        receive_ir();
+        
     }
     
 }
@@ -170,16 +186,19 @@ void generate_query() {
     create("query");
     while(clock < SIM_TIME){
         /* code */
+        hold(1);
     }
     
 }
 
-void receive_ir() {
+void receive_ir(long n) {
     create("receive_ir");
-    long i;
+    struct message *m;
+    m = &msg;
     while(clock < SIM_TIME){
-        /*receive(node, &msg);*/
-        /*printf("message received %ld", node[0].mbox);*/
+        hold(1);
+        receive(node[n].mbox, (long*)&m);
+        printf("Node %ld received %ld message\n", n, m->id);
     }
     
 }
