3c769559febf3fd27f4dab66de1d481d59b1c8cb
Jaturong Kongmanee <jaturongkongmanee@Jaturongs-MacBook-Pro.local>
2019-04-15 00:29:40 -0500

    Send data_list message

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 */
 
 /* #define is used to define CONSTANT */
-#define SIM_TIME 20.0
+#define SIM_TIME 50.0
 #define NUM_CLIENTS 2l
 #define NUM_SERVER 1l
 #define NUM_BOXES 3
@@ -37,11 +37,12 @@ struct message {
 
 };
 
-struct message data_list[100];
+struct message data_list[10];
+struct message *ptr[10];
 
 
-struct message *msg;
-struct message *m;
+/*struct message *msg;
+struct message *m;*/
 
 
 void init();
@@ -60,14 +61,7 @@ void receive_ir();
 void sim() {
     create("sim");
     init();
-    /*server();
-    client();*/
     hold(SIM_TIME);
-    /*while(clock < SIM_TIME){
-        server();
-        client();
-    }*/
-    
 }
 
 void init() {
@@ -75,21 +69,21 @@ void init() {
     char str[24];
     
     for (i = 0; i < 100; i++) {
-         data_list[i].id = i;
-         data_list[i].last_updated_time = 0;
+        data_list[i].id = i;
+        data_list[i].last_updated_time = 0;
+        ptr[i] = &data_list[i];
+        printf("Test pointer %ld\n", ptr[i]->id);
     }
 
     long all_node = NUM_CLIENTS + NUM_CLIENTS;
-    /*max_mailboxes(all_node);*/
-    /*max_servers(all_node);
-    max_events(all_node);*/
 
     for (i = 0; i < all_node; i++) {
         sprintf(str, "input.%d", i);
         node[i].mbox = mailbox(str);
     }
 
-    /*create_mailbox_set("mbox set", mbox_arr, NUM_BOXES);*/
+    data_list[0].id = 555;
+    /*printf("Test pointer %ld\n", ptr[0]->id);*/
 
     server(0);
     printf("Calling server\n");
@@ -97,37 +91,40 @@ void init() {
     for (i = 0; i < NUM_CLIENTS; i++) {
         client(i);
         printf("Calling client %ld\n", i);
-    }
-    
+    }    
 }
 
 void server(long n) {
     create("server");
     printf("Server is generated\n");
+    printf("Calling IR\n");
     invalidation_report();
+    /*update_data_items();*/
+    /*receive_message();*/
     while(clock < SIM_TIME){
-        hold(5);
-        printf("Calling IR\n");
+        hold(1);
         
-        
-        /*update_data_items();
-        receive_message();*/
     }   
 }
 
 void invalidation_report() {
-    create("ir");
+    create("ir"); 
     printf("IR is generated\n");
     while(clock < SIM_TIME){
-        hold(5);
+        hold(5);    
         /* Broadcast: put the message into all clients' mailboxes */
         /*struct message *msg;*/
-        msg = (struct message*)malloc(sizeof(*msg));
+        long i;
+        /*for (i = 0; i < 100; i++) {
+            data_list[i].last_updated_time = 88888;
+        }*/
 
-        long j;
-        for (j = 0; j < 100; j++) {
-            data_list[j].last_updated_time = 1;
-        }
+        /*printf("pppp\n");
+        printf(ptr[0]->id);
+        printf("uuuu\n");*/
+        
+
+        
             
         /*send(node[0].mbox, (long)msg);
         printf("sent message to node %ld\n", 0);
@@ -139,15 +136,10 @@ void invalidation_report() {
         printf("message received %ld \n", node[0].mbox);
         receive(node[1].mbox, (long*)&m);
         printf("message received %ld \n", node[1].mbox);*/
-        long i;
         for (i = 0; i < NUM_CLIENTS; i++) {
-            /* Transmission delay */
-            /* hold(2); */
-            /*msg->id = i+1000;*/
-            send(node[i].mbox, (long)data_list);
-            printf("sent message to node %ld with %ld\n", i, data_list);
-            /*receive(node[i].mbox, (long*)&m);
-            printf("message received %ld \n", m->id);*/
+            printf("sending %ld \n", data_list[i].id);
+            send(node[i].mbox, data_list);
+            printf("sent message to node %ld with %ld\n", i, data_list[i].id);
         }
     }
     
@@ -155,9 +147,14 @@ void invalidation_report() {
 
 void update_data_items() {
     create("update");
+    printf("Updating data items\n");
     while(clock < SIM_TIME){
         /* code */
-        hold(1);
+        hold(10);
+        long i;
+        for (i = 0; i < 100; i++) {
+            ptr[i]->last_updated_time = 88888;
+        }
     }
     
 }
@@ -171,15 +168,9 @@ void receive_message() {
 }
 
 
-
-
-
-
-
-
 void client(long n) {
     create("client");
-    printf("Client %ld is generatedddd\n", n);
+    printf("Client %ld is generated\n", n);
     receive_ir(n);
     while(clock < SIM_TIME){
         hold(1);
@@ -200,12 +191,11 @@ void generate_query() {
 
 void receive_ir(long n) {
     create("receive_ir");
-    struct message *m;
-    m = &msg;
+    printf("receiving messages\n");
     while(clock < SIM_TIME){
-        hold(1);
-        receive(node[n].mbox, (long*)&m);
-        printf("Node %ld received %ld message\n", n, m->id);
+        hold(5);
+        receive(node[n].mbox, ptr);
+        printf("Node %ld received message\n", n);
     }
     
 }
